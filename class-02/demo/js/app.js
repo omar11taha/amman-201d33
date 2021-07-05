a675da3d62c1279f61443c716de8b14d737815da
samer <s.allaham@ltuc.com>
2021-07-05 12:43:34 +0300

    pushing class2 demo

---
+++ class-02/demo/js/app.js
@@ -0,0 +1,197 @@
+'use strict';
+
+// let userName='samer';
+
+// // alert('hello '+userName);
+
+// console.log('hello',userName);
+
+// comments
+// this is a comment for one line
+
+/*
+
+to write multible lines of comments
+
+*/
+
+
+/* Data types:
+
+
+-String:
+any sympol inside 'single quotes or "double quotes"
+
+
+- Number
+5 integer , 3.2 Float
+
+-Boolean
+true or false
+
+*/
+
+// let userName='ahmad';
+// console.log(userName);
+
+// let age=prompt('whats your age');
+// console.log(age);
+
+// check the data type of a variable:
+
+// console.log(typeof age);
+
+// change the string to a number
+
+// age=parseInt(age);
+
+// age=Number(age);
+// console.log(age+100);
+
+
+/*
+If statement:
+
+if (condition){
+    code will run if condition is true
+}else if(another condition){
+    the other code  will run if other condition is true
+}else{
+    if none of them is true run this code
+}
+
+
+
+
+
+
+*/
+
+
+// let feeling=confirm('are you good?');
+// console.log(feeling);
+
+// if(feeling===true){
+//     console.log('glad you\'re good');
+// }else{
+//     console.log('hope you feel better soon');
+// }
+
+
+// let favouriteColor=prompt('what do you like more black/red?');
+
+// console.log('the answer before',favouriteColor);
+
+// favouriteColor=favouriteColor.toLowerCase();
+
+// console.log('the answer after',favouriteColor);
+
+// if (favouriteColor==='black') {
+
+//     console.log('black is your favourite color');
+alert('black is your faourite');
+
+// } else if(favouriteColor==='red'){
+
+//     console.log('red is your favourite color');
+
+// }else {
+//     alert('please write black or red');
+// }
+
+
+
+/*
+Switch:
+
+
+switch(variable){
+
+    case'value 1':
+      do code.. 
+      break;
+
+    case 'value2':
+        do code.. 
+        break;
+
+    default:
+        do if nothing matches cases
+        break;
+
+}
+
+
+
+*/
+
+
+// let fruit = prompt('what is your faourite fruit?');
+
+// console.log('before', fruit);
+
+// fruit = fruit.toUpperCase();
+
+// console.log('after', fruit);
+
+
+// switch (fruit) {
+
+//     case 'APPLE':
+//         alert('apples are 5 dinars a kilo');
+//         break;
+
+//     case 'BANANA':
+//     case 'MELON':
+
+//         alert('bananas and melons are 2 dinars a kilo');
+//         break;
+
+//     default:
+//         alert('sorry we dont have that right now');
+//         break;
+
+// }
+
+
+/*
+GitHub:
+Online
+the cloud
+stores code online and we can share it with other developers
+
+
+Git:
+local machine
+VCS
+
+*/
+
+
+// AND
+// is var1 and var2 true?
+/*
+true && true =>true
+true && flase=>false
+flase && true=>false
+flase && false=>false
+
+
+*/
+
+
+// OR
+
+// is var1 or var2 true?
+/*
+
+true && true =>true
+true && flase=>true
+flase && true=>true
+flase && false=>false
+
+
+*/
+
+
+
