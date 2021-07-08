efcb6092e6585c05c0a61d1fbe23ee5b22b6e33e
samer <s.allaham@ltuc.com>
2021-07-08 12:00:00 +0300

    pushing demo

---
+++ class-05/demo/app.js
@@ -0,0 +1,94 @@
+'use strict';
+
+// functions
+/*
+
+are a blocks of code the do something
+
+why:
+
+1)Reusability:
+use same code more than once 
+
+2)deviding code into smaller parts.
+
+
+*/
+// funtion declaration
+
+// let userName='ahmad';
+
+
+// console.log(welcomeMessage('ahmad'));
+// function welcomeMessage(userName){
+
+//     let output='welcome to my page ' +userName;
+//     return output;
+    // any code doesnt work 
+// }
+// parathensies
+// let car;
+
+
+
+// console.log(welcomeMessage('suhaib'));
+// console.log(welcomeMessage('reem'));
+// console.log(welcomeMessage('alaa'));
+
+
+
+// function expression
+
+// console.log(sum(5,9));
+// let sum=function(num1,num2){
+//    let result=num1+num2;
+//    return result;
+
+// }
+// sum();
+
+// console.log(sum(100,22));
+// console.log(sum(3,90));
+
+
+
+
+// let car;
+
+// car='bmw';
+
+// console.log(car);
+
+// var car='bmw';
+
+
+// Immediatly invoked function:
+
+// (function (){
+//     alert('hello');
+// })();
+
+
+
+// Scopes
+
+// let  globalVar='im a global var';
+
+// if (true) {
+    // local scope
+//     let localVariable='im a local variable';
+//     console.log(globalVar);
+// }
+// console.log(globalVar);
+
+// function getAge() {
+//     // local scope
+//     let message='your age is '
+//     let year=2021-1995;
+//     return [year,message];
+// }
+// console.log(getAge());
+// console.log(getAge()[1]);
+// console.log(getAge()[0]);
+
+
